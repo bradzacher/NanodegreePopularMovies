6a5e4ba15238eb577071f371b510a5e20ec9b330
Brad Zacher <brad.zacher@gmail.com>
2015-07-11 18:48:38 +0930

    missed this in the commit

---
+++ app/src/main/java/au/com/zacher/popularmovies/api/TheMovieDbService.java
@@ -0,0 +1,253 @@
+package au.com.zacher.popularmovies.api;
+
+import java.util.Map;
+
+import au.com.zacher.popularmovies.api.results.CertificationResults;
+import au.com.zacher.popularmovies.api.results.DatedPagedResults;
+import au.com.zacher.popularmovies.api.results.GenreResults;
+import au.com.zacher.popularmovies.api.results.PagedResults;
+import au.com.zacher.popularmovies.api.results.RelatedPagedResults;
+import au.com.zacher.popularmovies.api.results.RelatedResults;
+import au.com.zacher.popularmovies.model.Company;
+import au.com.zacher.popularmovies.model.Keyword;
+import au.com.zacher.popularmovies.model.Movie;
+import au.com.zacher.popularmovies.model.MovieVideo;
+import au.com.zacher.popularmovies.model.Review;
+import au.com.zacher.popularmovies.model.SimpleMovie;
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.Path;
+import retrofit.http.Query;
+import retrofit.http.QueryMap;
+
+/**
+ * Created by Brad on 10/07/2015.
+ */
+public interface TheMovieDbService {
+    public interface ConfigurationService {
+        @GET("/configuration")
+        public void get(Callback<Configuration> callback);
+    }
+
+    public interface CertificationsService {
+        @GET("/certification/movie/list")
+        public void getMovieList(Callback<CertificationResults> callback);
+
+        @GET("/certification/tv/list")
+        public void getTvList(Callback<CertificationResults> callback);
+    }
+
+    // tODO: maybe include collections?
+   /*public interface CollectionsService {
+        @GET("collection/{id}")
+        public void getCollection(@Path("id") int id, Callback<Object> callback);
+
+        @GET("collection/{id}/images")
+        public void getImages(@Path("id") int id, Callback<Object> callback);
+
+        @GET("collection/{id}?append_to_response=images")
+        public void getCollectionAndImages(@Path("id") int id, Callback<Object> callback);
+    }*/
+
+    public interface CompaniesService {
+        @GET("/company/{id}")
+        public void getCompany(@Path("id") int id, Callback<Company> callback);
+
+        @GET("/company/{id}/movies")
+        public void getCompanyMovies(@Path("id") int id, Callback<RelatedPagedResults<SimpleMovie>> callback);
+        @GET("/company/{id}/movies")
+        public void getCompanyMovies(@Path("id") int id, @Query("page") int pageNum, Callback<RelatedPagedResults<SimpleMovie>> callback);
+    }
+
+    public interface DiscoverService {
+        @GET("/discover/movie")
+        public void getMovieList(Callback<PagedResults<SimpleMovie>> callback);
+        @GET("/discover/movie")
+        public void getMovieList(@QueryMap Map<String, Object> map, Callback<PagedResults<SimpleMovie>> callback);
+
+        //@GET("/discover/tv")
+        //public void getTvList(Callback<Object> callback);
+        //@GET("/discover/tv")
+        //public void getTvList(@QueryMap Map<String, Object> map, Callback<Object> callback);
+    }
+
+    public interface GenresService {
+        @GET("/genre/movie/list")
+        public void getMovieList(Callback<GenreResults> callback);
+
+        @GET("/genre/tv/list")
+        public void getTvList(Callback<GenreResults> callback);
+
+        @GET("/genre/{id}/movies")
+        public void getMoviesForGenre(@Path("id") int id, Callback<RelatedPagedResults<SimpleMovie>> callback);
+        @GET("/genre/{id}/movies")
+        public void getMoviesForGenre(@Path("id") int id, @QueryMap Map<String, Object> map, Callback<RelatedPagedResults<SimpleMovie>> callback);
+    }
+
+    public interface KeywordService {
+        @GET("/keyword/{id}")
+        public void getKeyword(@Path("id") int id, Callback<Keyword> callback);
+
+        @GET("/keyword/{id}/movies")
+        public void getKeywordMovies(@Path("id") int id, Callback<RelatedPagedResults<SimpleMovie>> callback);
+    }
+
+    public interface MoviesService {
+        @GET("/movie/{id}")
+        public void getMovie(@Path("id") int id, Callback<Movie> callback);
+
+        //@GET("/movie/{id}")
+        //public void getMovieWith(@Path("id") int id, @Query("append_to_response") String commaSeparatedList, Callback<Object> callback);
+
+        //@GET("/movie/{id}/alternative_titles")
+        //public void getMovieAlternativeTitles(@Path("id") int id, Callback<Object> callback);
+
+        //@GET("/movie/{id}/credits")
+        //public void getMovieCredits(@Path("id") int id, Callback<Object> callback);
+
+        //@GET("/movie/{id}/images")
+        //public void getMovieImages(@Path("id") int id, Callback<Object> callback);
+
+        //@GET("/movie/{id}/keywords")
+        //public void getMovieKeywords(@Path("id") int id, Callback<Object> callback);
+
+        //@GET("/movie/{id}/releases")
+        //public void getMovieReleases(@Path("id") int id, Callback<Object> callback);
+
+        @GET("/movie/{id}/videos")
+        public void getMovieVideos(@Path("id") int id, Callback<RelatedResults<MovieVideo>> callback);
+
+        //@GET("/movie/{id}/translations")
+        //public void getMovieTranslations(@Path("id") int id, Callback<Object> callback);
+
+        @GET("/movie/{id}/similar")
+        public void getSimilarMovies(@Path("id") int id, Callback<PagedResults<SimpleMovie>> callback);
+
+        @GET("/movie/{id}/reviews")
+        public void getMovieReviews(@Path("id") int id, Callback<RelatedPagedResults<Review>> callback);
+
+        //@GET("/movie/latest")
+        //public void getLatest(Callback<Object> callback);
+
+        @GET("/movie/now_playing")
+        public void getNowPlaying(Callback<DatedPagedResults<SimpleMovie>> callback);
+        @GET("/movie/now_playing")
+        public void getNowPlaying(@Query("page") int pageNum, Callback<DatedPagedResults<SimpleMovie>> callback);
+
+        @GET("/movie/popular")
+        public void getPopular(Callback<PagedResults<SimpleMovie>> callback);
+        @GET("/movie/popular")
+        public void getPopular(@Query("page") int pageNum, Callback<PagedResults<SimpleMovie>> callback);
+
+        @GET("/movie/top_rated")
+        public void getTopRated(Callback<PagedResults<SimpleMovie>> callback);
+        @GET("/movie/top_rated")
+        public void getTopRated(@Query("page") int pageNum, Callback<PagedResults<SimpleMovie>> callback);
+
+        @GET("/movie/upcoming")
+        public void getUpcoming(Callback<DatedPagedResults<SimpleMovie>> callback);
+        @GET("/movie/upcoming")
+        public void getUpcoming(@Query("page") int pageNum, Callback<DatedPagedResults<SimpleMovie>> callback);
+    }
+
+    public interface ReviewsService {
+        @GET("/review/{id}")
+        public void getReview(@Path("id") int id, Callback<Review> callback);
+    }
+
+    // TODO: maybe include search?
+    /*public interface SearchService {
+        @GET("/search/company")
+        public void company(@Query("query") String query, Callback<Object> callback);
+        @GET("/search/company")
+        public void company(@Query("query") String query, @Query("page") int pageNum, Callback<Object> callback);
+
+        @GET("/search/collection")
+        public void collection(@Query("query") String query, Callback<Object> callback);
+        @GET("/search/collection")
+        public void collection(@Query("query") String query, @Query("page") int pageNum, Callback<Object> callback);
+
+        @GET("/search/keyword")
+        public void keyword(@Query("query") String query, Callback<Object> callback);
+        @GET("/search/keyword")
+        public void keyword(@Query("query") String query, @Query("page") int pageNum, Callback<Object> callback);
+
+        @GET("/search/movie")
+        public void movie(@Query("query") String query, Callback<Object> callback);
+        @GET("/search/movie")
+        public void movie(@Query("query") String query, @QueryMap Map<String, Object> map, Callback<Object> callback);
+        @GET("/search/movie")
+        public void movie(@Query("query") String query, @Query("page") int pageNum, @QueryMap Map<String, Object> map, Callback<Object> callback);
+
+        @GET("/search/tv")
+        public void tv(@Query("query") String query, Callback<Object> callback);
+        @GET("/search/tv")
+        public void tv(@Query("query") String query, @QueryMap Map<String, Object> map, Callback<Object> callback);
+        @GET("/search/tv")
+        public void tv(@Query("query") String query, @Query("page") int pageNum, @QueryMap Map<String, Object> map, Callback<Object> callback);
+    }*/
+
+    // TODO: maybe extend to tv?
+    /*public interface TVService {
+        @GET("/tv/{id}")
+        public void getTv(@Path("id") int id, Callback<Object> callback);
+
+        //@GET("/tv/{id}")
+        //public void getTvWith(@Path("id") int id, @Query("append_to_response") String commaSeparatedList, Callback<Object> callback);
+
+        //@GET("/tv/{id}/alternative_titles")
+        //public void getTvAlternativeTitles(@Path("id") int id, Callback<Object> callback);
+
+        //@GET("/tv/{id}/credits")
+        //public void getTvCredits(@Path("id") int id, Callback<Object> callback);
+
+        //@GET("/tv/{id}/images")
+        //public void getTvImages(@Path("id") int id, Callback<Object> callback);
+
+        //@GET("/tv/{id}/keywords")
+        //public void getTvKeywords(@Path("id") int id, Callback<Object> callback);
+
+        //@GET("/tv/{id}/releases")
+        //public void getTvReleases(@Path("id") int id, Callback<Object> callback);
+
+        @GET("/tv/{id}/videos")
+        public void getTvVideos(@Path("id") int id, Callback<Object> callback);
+
+        //@GET("/tv/{id}/translations")
+        //public void getTvTranslations(@Path("id") int id, Callback<Object> callback);
+
+        @GET("/tv/{id}/similar")
+        public void getSimilarTv(@Path("id") int id, Callback<Object> callback);
+
+        @GET("/tv/{id}/reviews")
+        public void getTvReviews(@Path("id") int id, Callback<Object> callback);
+
+        @GET("/tv/latest")
+        public void getLatest(Callback<Object> callback);
+
+        @GET("/tv/on_the_air")
+        public void getOnTheAir(Callback<Object> callback);
+        @GET("/tv/on_the_air")
+        public void getOnTheAir(@Query("page") int pageNum, Callback<Object> callback);
+
+        @GET("/tv/airing_today")
+        public void getAiringToday(Callback<Object> callback);
+        @GET("/tv/airing_today")
+        public void getAiringToday(@Query("page") int pageNum, Callback<Object> callback);
+
+        @GET("/tv/popular")
+        public void getPopular(Callback<Object> callback);
+        @GET("/tv/popular")
+        public void getPopular(@Query("page") int pageNum, Callback<Object> callback);
+
+        @GET("/tv/top_rated")
+        public void getTopRated(Callback<Object> callback);
+        @GET("/tv/top_rated")
+        public void getTopRated(@Query("page") int pageNum, Callback<Object> callback);
+
+        @GET("/tv/upcoming")
+        public void getUpcoming(Callback<Object> callback);
+        @GET("/tv/upcoming")
+        public void getUpcoming(@Query("page") int pageNum, Callback<Object> callback);
+    }*/
+}
